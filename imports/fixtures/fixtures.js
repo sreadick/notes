6715a2ce89424f400158b1dc543dc5d182672d56
Sky Readick <szreadick@gmail.com>
2017-07-15 01:55:24 -0400

    Integrate selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,15 @@
+export const notes = [
+  {
+    _id: "noteId1",
+    title: 'Test title',
+    body: '',
+    updatedAt: 1500083884534,
+    userId: 'userId1'
+  }, {
+    _id: "noteId2",
+    title: '',
+    body: 'something is here',
+    updatedAt: 1500083884534,
+    userId: 'userId2'
+  }
+];
