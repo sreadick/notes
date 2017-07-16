1f0fb36b462c05154490a15bc76ac885a8f0c57c
Sky Readick <szreadick@gmail.com>
2017-07-15 23:09:42 -0400

    Refactor router to allow for dynamic urls

--- client/main.js
+++ client/main.js
@@ -9,12 +9,13 @@ import '../imports/startup/simple-schema-configuration';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  onAuthChange(isAuthenticated);
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+  
+  onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
 Tracker.autorun(() => {
   const selectedNoteId = Session.get('selectedNoteId');
-
   if (selectedNoteId) {
     browserHistory.replace(`/dashboard/${selectedNoteId}`);
   }
